6891f0e14e5c670cfc4e23770ed368e472b1abd6
gawalimr <gawalimr@gmail.com>
2021-10-16 13:53:27 +0530

    Requirement-1 twoNumbers_should_return_sum  test Passed

--- incuByteExam-master.zip_expanded/incuByteExam-master/src/main/java/calculator/StringCalculator.java
+++ incuByteExam-master.zip_expanded/incuByteExam-master/src/main/java/calculator/StringCalculator.java
@@ -1,14 +1,21 @@
 package calculator;
 
 class StringCalculator {
-
-	
 	 public int add(String input) { 
+		 String [] numbers=input.split(",");
 		 if(input.isEmpty()) {
 		 return 0;
 		 }
-		 return convertToInt(input);
+		 if(input.length()==1) {
+			 return convertToInt(input);
+		 }else {
+			 return Integer.parseInt(numbers[0])+Integer.parseInt(numbers[1]);
+		 }
+		
 	 }
+	 
+	 
+	 
 	 private int convertToInt(String input) {
 		 return Integer.parseInt(input);
 	 }
