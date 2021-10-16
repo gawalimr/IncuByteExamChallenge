015f04bc9c7f6c198692b6bbf09f89c79a051bdc
gawalimr <gawalimr@gmail.com>
2021-10-16 12:56:57 +0530

    Requirement-1 Refactor

--- incuByteExam-master.zip_expanded/incuByteExam-master/src/test/java/calculator/StringCalculatorShould.java
+++ incuByteExam-master.zip_expanded/incuByteExam-master/src/test/java/calculator/StringCalculatorShould.java
@@ -6,9 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
 
+	StringCalculator stringCalculator = new StringCalculator();
+	
     @Test
     void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
+        
         assertEquals(0, stringCalculator.add(""));
     }
     
