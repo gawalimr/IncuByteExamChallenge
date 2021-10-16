cdce8600e3b11b962e4da96de771bcdcb9861444
gawalimr <gawalimr@gmail.com>
2021-10-16 18:51:11 +0530

    Requirement-10 delimitersCanBeOfAnyLength test failed

--- incuByteExam-master.zip_expanded/incuByteExam-master/src/test/java/calculator/StringCalculatorShould.java
+++ incuByteExam-master.zip_expanded/incuByteExam-master/src/test/java/calculator/StringCalculatorShould.java
@@ -6,57 +6,63 @@ import junit.framework.Assert;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 class StringCalculatorShould {
 
-	StringCalculator stringCalculator=new StringCalculator();
-	
+	StringCalculator stringCalculator = new StringCalculator();
+
 	@Test
-    void empty_string_should_return_0() {        
-        assertEquals(0, stringCalculator.add(""));
-    }
-	
+	void empty_string_should_return_0() {
+		assertEquals(0, stringCalculator.add(""));
+	}
+
 	@Test
-    void singleValue_should_return_1() {        
-        assertEquals(1, stringCalculator.add("1"));
-    }
-	
+	void singleValue_should_return_1() {
+		assertEquals(1, stringCalculator.add("1"));
+	}
+
 	@Test
 	void twoNumbers_should_return_sum() {
 		assertEquals(3, stringCalculator.add("1,2"));
 	}
-	
+
 	@Test
 	void twoNumbers_DelimetedNewLine_should_return_sum() {
 		assertEquals(3, stringCalculator.add("1\n2"));
 	}
+
 	@Test
 	void ThreeNumbers_DelimetedNewLine_should_return_sum() {
 		assertEquals(6, stringCalculator.add("1\n2,3"));
 	}
-	
+
 	@Test
 	void negative_Number_Should_throw() {
 		stringCalculator.add("-1");
 	}
-	
+
 	@Test
 	void ignore_NumbersGT1000_should_return_sum() {
 		assertEquals(2, stringCalculator.add("2,1001"));
 	}
-	
+
 	@Test
-	public final void Negative_NumbersAreUsed_ExceptionIsThrown() {
-	    stringCalculator.add("3,-6,15,-18,46,33");
+	public void Negative_NumbersAreUsed_ExceptionIsThrown() {
+		stringCalculator.add("3,-6,15,-18,46,33");
 	}
-	
-	 @Test
-	    public final void whenAnyNumberOfNumbers_IsUsed_ThenReturnValuesAreTheirSums() {
-	        assertEquals(3+6+15+18+46+33, stringCalculator.add("3,6,15,18,46,33"));
-	 }
-	 @Test
-	 public final void whenDelimiterIsSpecifiedThenItIsUsedToSeparateNumbers() {
-	     assertEquals(3+6+15, stringCalculator.add("//;n3;6;15"));
-	 }
-	
+
+	@Test
+	public void whenAnyNumberOfNumbers_IsUsed_ThenReturnValuesAreTheirSums() {
+		assertEquals(3 + 6 + 15 + 18 + 46 + 33, stringCalculator.add("3,6,15,18,46,33"));
+	}
+
+	@Test
+	public void whenDelimiterIsSpecifiedThenItIsUsedToSeparateNumbers() {
+		assertEquals(3 + 6 + 15, stringCalculator.add("//;n3;6;15"));
+	}
+
+	@Test
+	public final void delimitersCanBeOfAnyLength() {
+		assertEquals(6, stringCalculator.add("//[***]\\n1***2***3"));
+	}
+
 }
