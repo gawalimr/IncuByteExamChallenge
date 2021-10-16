f075804972f7a306a5387b8f6cce8d6e871a7eeb
gawalimr <gawalimr@gmail.com>
2021-10-16 13:29:36 +0530

    Requirement-1 singleValue_should_return_1  test Failed

--- incuByteExam-master.zip_expanded/incuByteExam-master/src/test/java/calculator/StringCalculatorShould.java
+++ incuByteExam-master.zip_expanded/incuByteExam-master/src/test/java/calculator/StringCalculatorShould.java
@@ -4,14 +4,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class StringCalculatorShould {
 
 	StringCalculator stringCalculator = new StringCalculator();
 	
-    @Test
-    void empty_string_should_return_0() {
-        
+	@Test
+    void empty_string_should_return_0() {        
         assertEquals(0, stringCalculator.add(""));
+    }
+	
+	@Test
+    void singleValue_should_return_1() {        
+        assertEquals(1, stringCalculator.add("1"));
     }
     
 }
